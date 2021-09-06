b308d32097b9b26f99cfd3f8ea3c582587dda545
Alex Forencich <alex@alexforencich.com>
2021-09-05 18:20:11 -0700

    Update PCIe statistics increment widths

--- fpga/common/rtl/stats_pcie_if.v
+++ fpga/common/rtl/stats_pcie_if.v
@@ -200,20 +200,20 @@ stats_pcie_rx_cpl_tlp_inst (
     .stat_tlp_cpl_dw(stat_rx_cpl_tlp_cpl_dw)
 );
 
-wire [10:0] stat_rx_tlp_mem_rd = stat_rx_req_tlp_mem_rd;
-wire [10:0] stat_rx_tlp_mem_wr = stat_rx_req_tlp_mem_wr;
-wire [10:0] stat_rx_tlp_io = stat_rx_req_tlp_io;
-wire [10:0] stat_rx_tlp_cfg = stat_rx_req_tlp_cfg;
-wire [10:0] stat_rx_tlp_msg = stat_rx_req_tlp_msg;
-wire [10:0] stat_rx_tlp_cpl = stat_rx_cpl_tlp_cpl;
-wire [10:0] stat_rx_tlp_cpl_ur = stat_rx_cpl_tlp_cpl_ur;
-wire [10:0] stat_rx_tlp_cpl_ca = stat_rx_cpl_tlp_cpl_ca;
-wire [10:0] stat_rx_tlp_atomic = stat_rx_req_tlp_atomic;
-wire [10:0] stat_rx_tlp_ep = stat_rx_req_tlp_ep + stat_rx_cpl_tlp_ep;
-wire [10:0] stat_rx_tlp_hdr_dw = stat_rx_req_tlp_hdr_dw + stat_rx_cpl_tlp_hdr_dw;
-wire [10:0] stat_rx_tlp_req_dw = stat_rx_req_tlp_req_dw;
-wire [10:0] stat_rx_tlp_payload_dw = stat_rx_req_tlp_payload_dw;
-wire [10:0] stat_rx_tlp_cpl_dw = stat_rx_cpl_tlp_cpl_dw;
+wire [12:0] stat_rx_tlp_mem_rd_inc = stat_rx_req_tlp_mem_rd;
+wire [12:0] stat_rx_tlp_mem_wr_inc = stat_rx_req_tlp_mem_wr;
+wire [12:0] stat_rx_tlp_io_inc = stat_rx_req_tlp_io;
+wire [12:0] stat_rx_tlp_cfg_inc = stat_rx_req_tlp_cfg;
+wire [12:0] stat_rx_tlp_msg_inc = stat_rx_req_tlp_msg;
+wire [12:0] stat_rx_tlp_cpl_inc = stat_rx_cpl_tlp_cpl;
+wire [12:0] stat_rx_tlp_cpl_ur_inc = stat_rx_cpl_tlp_cpl_ur;
+wire [12:0] stat_rx_tlp_cpl_ca_inc = stat_rx_cpl_tlp_cpl_ca;
+wire [12:0] stat_rx_tlp_atomic_inc = stat_rx_req_tlp_atomic;
+wire [12:0] stat_rx_tlp_ep_inc = stat_rx_req_tlp_ep + stat_rx_cpl_tlp_ep;
+wire [12:0] stat_rx_tlp_hdr_dw_inc = stat_rx_req_tlp_hdr_dw + stat_rx_cpl_tlp_hdr_dw;
+wire [12:0] stat_rx_tlp_req_dw_inc = stat_rx_req_tlp_req_dw;
+wire [12:0] stat_rx_tlp_payload_dw_inc = stat_rx_req_tlp_payload_dw;
+wire [12:0] stat_rx_tlp_cpl_dw_inc = stat_rx_cpl_tlp_cpl_dw;
 
 wire stat_tx_rd_req_tlp_mem_rd;
 wire stat_tx_rd_req_tlp_mem_wr;
@@ -365,29 +365,29 @@ stats_pcie_tx_cpl_tlp_inst (
     .stat_tlp_cpl_dw(stat_tx_cpl_tlp_cpl_dw)
 );
 
-wire [10:0] stat_tx_tlp_mem_rd = stat_tx_rd_req_tlp_mem_rd;
-wire [10:0] stat_tx_tlp_mem_wr = stat_tx_wr_req_tlp_mem_wr;
-wire [10:0] stat_tx_tlp_io = 0;
-wire [10:0] stat_tx_tlp_cfg = 0;
-wire [10:0] stat_tx_tlp_msg = 0;
-wire [10:0] stat_tx_tlp_cpl = stat_tx_cpl_tlp_cpl;
-wire [10:0] stat_tx_tlp_cpl_ur = stat_tx_cpl_tlp_cpl_ur;
-wire [10:0] stat_tx_tlp_cpl_ca = stat_tx_cpl_tlp_cpl_ca;
-wire [10:0] stat_tx_tlp_atomic = 0;
-wire [10:0] stat_tx_tlp_ep = 0;
-wire [10:0] stat_tx_tlp_hdr_dw = stat_tx_rd_req_tlp_hdr_dw + stat_tx_wr_req_tlp_hdr_dw + stat_tx_cpl_tlp_hdr_dw;
-wire [10:0] stat_tx_tlp_req_dw = stat_tx_rd_req_tlp_req_dw;
-wire [10:0] stat_tx_tlp_payload_dw = stat_tx_wr_req_tlp_payload_dw;
-wire [10:0] stat_tx_tlp_cpl_dw = stat_tx_cpl_tlp_cpl_dw;
+wire [12:0] stat_tx_tlp_mem_rd_inc = stat_tx_rd_req_tlp_mem_rd;
+wire [12:0] stat_tx_tlp_mem_wr_inc = stat_tx_wr_req_tlp_mem_wr;
+wire [12:0] stat_tx_tlp_io_inc = 0;
+wire [12:0] stat_tx_tlp_cfg_inc = 0;
+wire [12:0] stat_tx_tlp_msg_inc = 0;
+wire [12:0] stat_tx_tlp_cpl_inc = stat_tx_cpl_tlp_cpl;
+wire [12:0] stat_tx_tlp_cpl_ur_inc = stat_tx_cpl_tlp_cpl_ur;
+wire [12:0] stat_tx_tlp_cpl_ca_inc = stat_tx_cpl_tlp_cpl_ca;
+wire [12:0] stat_tx_tlp_atomic_inc = 0;
+wire [12:0] stat_tx_tlp_ep_inc = 0;
+wire [12:0] stat_tx_tlp_hdr_dw_inc = stat_tx_rd_req_tlp_hdr_dw + stat_tx_wr_req_tlp_hdr_dw + stat_tx_cpl_tlp_hdr_dw;
+wire [12:0] stat_tx_tlp_req_dw_inc = stat_tx_rd_req_tlp_req_dw;
+wire [12:0] stat_tx_tlp_payload_dw_inc = stat_tx_wr_req_tlp_payload_dw;
+wire [12:0] stat_tx_tlp_cpl_dw_inc = stat_tx_cpl_tlp_cpl_dw;
 
 stats_collect #(
     .COUNT(32),
-    .INC_WIDTH(11),
+    .INC_WIDTH(13),
     .STAT_INC_WIDTH(STAT_INC_WIDTH),
     .STAT_ID_WIDTH(5),
     .UPDATE_PERIOD(UPDATE_PERIOD)
 )
-stats_collect_tx_inst (
+stats_collect_inst (
     .clk(clk),
     .rst(rst),
 
@@ -395,38 +395,38 @@ stats_collect_tx_inst (
      * Increment inputs
      */
     .stat_inc({
-        11'd0,                   // index 31
-        11'd0,                   // index 30
-        stat_tx_tlp_cpl_dw,      // index 29
-        stat_tx_tlp_payload_dw,  // index 28
-        stat_tx_tlp_req_dw,      // index 27
-        stat_tx_tlp_hdr_dw,      // index 26
-        stat_tx_tlp_ep,          // index 25
-        stat_tx_tlp_atomic,      // index 24
-        stat_tx_tlp_cpl_ca,      // index 23
-        stat_tx_tlp_cpl_ur,      // index 22
-        stat_tx_tlp_cpl,         // index 21
-        stat_tx_tlp_msg,         // index 20
-        stat_tx_tlp_cfg,         // index 19
-        stat_tx_tlp_io,          // index 18
-        stat_tx_tlp_mem_wr,      // index 17
-        stat_tx_tlp_mem_rd,      // index 16
-        11'd0,                   // index 15
-        11'd0,                   // index 14
-        stat_rx_tlp_cpl_dw,      // index 13
-        stat_rx_tlp_payload_dw,  // index 12
-        stat_rx_tlp_req_dw,      // index 11
-        stat_rx_tlp_hdr_dw,      // index 10
-        stat_rx_tlp_ep,          // index 9
-        stat_rx_tlp_atomic,      // index 8
-        stat_rx_tlp_cpl_ca,      // index 7
-        stat_rx_tlp_cpl_ur,      // index 6
-        stat_rx_tlp_cpl,         // index 5
-        stat_rx_tlp_msg,         // index 4
-        stat_rx_tlp_cfg,         // index 3
-        stat_rx_tlp_io,          // index 2
-        stat_rx_tlp_mem_wr,      // index 1
-        stat_rx_tlp_mem_rd       // index 0
+        13'd0,                       // index 31
+        13'd0,                       // index 30
+        stat_tx_tlp_cpl_dw_inc,      // index 29
+        stat_tx_tlp_payload_dw_inc,  // index 28
+        stat_tx_tlp_req_dw_inc,      // index 27
+        stat_tx_tlp_hdr_dw_inc,      // index 26
+        stat_tx_tlp_ep_inc,          // index 25
+        stat_tx_tlp_atomic_inc,      // index 24
+        stat_tx_tlp_cpl_ca_inc,      // index 23
+        stat_tx_tlp_cpl_ur_inc,      // index 22
+        stat_tx_tlp_cpl_inc,         // index 21
+        stat_tx_tlp_msg_inc,         // index 20
+        stat_tx_tlp_cfg_inc,         // index 19
+        stat_tx_tlp_io_inc,          // index 18
+        stat_tx_tlp_mem_wr_inc,      // index 17
+        stat_tx_tlp_mem_rd_inc,      // index 16
+        13'd0,                       // index 15
+        13'd0,                       // index 14
+        stat_rx_tlp_cpl_dw_inc,      // index 13
+        stat_rx_tlp_payload_dw_inc,  // index 12
+        stat_rx_tlp_req_dw_inc,      // index 11
+        stat_rx_tlp_hdr_dw_inc,      // index 10
+        stat_rx_tlp_ep_inc,          // index 9
+        stat_rx_tlp_atomic_inc,      // index 8
+        stat_rx_tlp_cpl_ca_inc,      // index 7
+        stat_rx_tlp_cpl_ur_inc,      // index 6
+        stat_rx_tlp_cpl_inc,         // index 5
+        stat_rx_tlp_msg_inc,         // index 4
+        stat_rx_tlp_cfg_inc,         // index 3
+        stat_rx_tlp_io_inc,          // index 2
+        stat_rx_tlp_mem_wr_inc,      // index 1
+        stat_rx_tlp_mem_rd_inc       // index 0
     }),
     .stat_valid({32{1'b1}}),
 
